9955a8ee021750b8c57a6b84fd1a71b7c7e49b04
AntSegJim <antonio.alandalus97@gmail.com>
2020-05-31 01:32:23 +0200

    Update SendMailController.java

--- src/main/java/com/prosubject/prosubject/backend/apirest/controller/SendMailController.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/controller/SendMailController.java
@@ -17,7 +17,7 @@ import com.prosubject.prosubject.backend.apirest.service.SendMailService;
 
 @RestController
 @RequestMapping("/api/mail")
-@CrossOrigin(origins = { "http://localhost:4200", "https://prosubject-lanzamiento.herokuapp.com" })
+@CrossOrigin(origins = { "http://localhost:4200", "https://prosubject-final.herokuapp.com" })
 public class SendMailController {
 
 	@Autowired
