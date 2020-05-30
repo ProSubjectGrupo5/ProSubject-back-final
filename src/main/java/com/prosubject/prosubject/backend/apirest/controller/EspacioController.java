3a705452b3617ae568a5584e73f3f462f8397273
AntSegJim <antonio.alandalus97@gmail.com>
2020-05-31 01:27:59 +0200

    Update EspacioController.java

--- src/main/java/com/prosubject/prosubject/backend/apirest/controller/EspacioController.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/controller/EspacioController.java
@@ -30,7 +30,7 @@ import com.prosubject.prosubject.backend.apirest.service.ProfesorService;
 
 @RestController
 @RequestMapping("/api/espacios")
-@CrossOrigin(origins = {"http://localhost:4200", "https://prosubject-lanzamiento.herokuapp.com"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://prosubject-final.herokuapp.com"})
 public class EspacioController{
 
 	
