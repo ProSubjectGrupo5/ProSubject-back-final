284f875996017cf0a17bd492a6282b2e86ff62db
AntSegJim <antonio.alandalus97@gmail.com>
2020-05-31 01:33:03 +0200

    Update UniversidadController.java

--- src/main/java/com/prosubject/prosubject/backend/apirest/controller/UniversidadController.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/controller/UniversidadController.java
@@ -21,7 +21,7 @@ import com.prosubject.prosubject.backend.apirest.service.UniversidadService;
 
 @RestController
 @RequestMapping("/api/universidades")
-@CrossOrigin(origins = {"http://localhost:4200", "https://prosubject-lanzamiento.herokuapp.com"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://prosubject-final.herokuapp.com"})
 public class UniversidadController {
 
 	
